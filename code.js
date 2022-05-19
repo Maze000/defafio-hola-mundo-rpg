b7ca263507ada1ec644a4135d4dbe05226a78174
MAZE <72741681+Maze000@users.noreply.github.com>
2022-05-19 22:58:38 +1200

    Update code.js

--- code.js
+++ code.js
@@ -863,7 +863,6 @@ boton.addEventListener("click", intro00);
                             document.getElementsByTagName("body")[0].style.cursor = "url('./img/combo1.png'), auto";
                         }
                         if(aumento3<=0){
-                            ctx.clearRect(0,0,canvas.width,30);
                             setTimeout(finalEn,1000);
                         }
                         
